6fe571c9413075aae3a0426bd4b88d2dfdf2399e
lingxialingdu <1784538005@qq.com>
2016-11-01 22:54:58 +0800

    Revert "Add files via upload"
    This reverts commit bcfc43651f2589f7a6939ddb4dd6c29245633000.

--- interfaceObjectCode.h
+++
@@ -1,8 +0,0 @@
-#ifndef INTERFACEOBJECTCODE_H_INCLUDED
-#define INTERFACEOBJECTCODE_H_INCLUDED
-
-#include "middleCodeGen.h"
-void genFinalCode(CodeListNode *);
-void stepTM(void);
-void printObjectCode(void);
-#endif // INTERFACEOBJECTCODE_H_INCLUDED
